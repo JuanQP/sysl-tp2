1e9922a29254afe8840c755a7d45c2bed3fc0dca
JuanQP <quinterosp.juan@gmail.com>
2015-09-10 21:54:17 -0300

    Se agregan fuentes

---
+++ Fuentes/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*Sintaxis y Semantica de los Lenguajes: TP2
+Curso: Viernes-Noche*/
+
+int main()
+{
+    /*Codigo de la aplicacion.*/
+}
---
+++ Fuentes/scanner.h
@@ -0,0 +1,15 @@
+#ifndef SCANNER_H_INCLUDED
+#define SCANNER_H_INCLUDED
+
+typedef enum { INICIO, FIN, LEER, ESCRIBIR, ID, CONSTANTE, PARENIZQUIERDO,
+		PARENDERECHO, PUNTOYCOMA, COMA, ASIGNACION, SUMA, RESTA,
+		MULTIPLICACION, DIVISION, COMENTARIO, FDT,ERRORLEXICO,
+		ERRORASIG, ERRORCTE
+}token;
+
+extern char * const yytext;
+extern char *tok_names[];
+token prox_token(void);
+void match(token tok);
+
+#endif // SCANNER_H_INCLUDED
