6429e6bf7eccd0dca42f05455312cb0f1122dd17
JuanQP <quinterosp.juan@gmail.com>
2015-09-16 18:05:11 -0300

    Se agrega funcionalidad para leer lexemas y mostrar tokens Fixes #1, Fixes #2

--- Fuentes/main.c
+++ Fuentes/main.c
@@ -4,7 +4,41 @@
 /*Sintaxis y Semantica de los Lenguajes: TP2
 Curso: Viernes-Noche*/
 
+/*Definicion de prototipos.*/
+void encabezado();
+
 int main()
 {
-    /*Codigo de la aplicacion.*/
+    /*Guardo los nombres de los tokens.*/
+    char *nombresToken[20] = {"Inicio","Fin","Leer","Escribir",
+        "Identificador","Constante","ParenIzquierdo","ParenDerecho",
+        "PuntoYComa", "Coma", "Asignacion", "Suma",
+        "Resta", "Multiplicacion", "Division", "Comentario",
+        "FinDeTexto", "ErrorLexico", "ErrorAsignacion", "ErrorConstante"};
+    
+    /*Guardo el token actual (numero).*/
+    token tokenActual;
+    
+    encabezado();
+
+    /*Llamo al Scanner para que recorra el archivo de texto.*/
+    while(tokenActual != FDT)
+    {
+        /*Llamo a scanner para que lea el siguiente lexema y me devuelva
+        el token correspondiente.*/
+        tokenActual = scanner();
+        
+        /*Imprimo la informacion correspondiente al token leido.*/
+        printf("Lexema: %s\n", yytext);
+        printf("Token : %s\n", nombresToken[tokenActual]);
+        printf("\n");
+    }
+}
+
+/*Imprime un bonito encabezado al principio del programa :v*/
+void encabezado()
+{
+    printf("\n");
+    printf("Sintaxis y Semantica de Lenguajes - UTN K2055 Grupo 3\n");
+    printf("COMPILADOR DE LENGUAJE MICRO - Scanner\n\n");
 }
--- Fuentes/scanner.c
+++ Fuentes/scanner.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "scanner.h"
+#include <ctype.h>
 
 static char buffer[100];
 char *const yytext = buffer;
+int columna(char);
+int esAceptor(int);
+token tokenCorrespondiente(int);
 
 token prox_token()
 {
@@ -18,5 +22,115 @@ void match(token tok)
 
 token scanner()
 {
-    /*Codigo del scanner.*/
+    int i = 0;
+    int estado = 0;
+    int proximoEstado = 0;
+    char letra = '\0';
+    
+    /*Defino la tabla de transición (Leer documentación).*/
+    int tablaTransicion[15][13] = 
+    {
+    { 1, 3, 5, 6, 7, 8, 9,10,11,14,13, 0,14},
+    { 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    { 4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {14,14,14,14,14,14,14,14,14,12,14,14,14},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,99,99,99,99}
+    };   
+    
+    letra = getchar();
+
+    /*Mientras no se haya reconocido FDT.*/
+    while(estado != 13)
+    {
+        proximoEstado = tablaTransicion[estado][columna(letra)];
+        if (esAceptor(proximoEstado))
+        {
+            estado = proximoEstado;
+            ungetc(letra, stdin);
+            break;
+        }
+        estado = proximoEstado;
+        
+        /*Los espacios se ignoran.*/
+        if(letra != ' ' && letra != '\n')
+        {
+            buffer[i] = letra;
+            i++;
+        }
+        letra = getchar();
+    }
+    
+    buffer[i]='\0';
+    return tokenCorrespondiente(estado);
+}
+
+/*Informa si el estado pasado por parametro es estado aceptor.*/
+int esAceptor(int estado)
+{
+   switch(estado)
+    {
+        case 2:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 12:
+        case 13:
+            return 1;
+            break;
+        default:
+            break;
+    }
+    
+    /*Llegado a esta instancia devuelve falso.*/
+    return 0;
+}
+
+/*Devuelve el indice de la columna que le corresponde al caracter.*/
+int columna(char letra)
+{
+    if(isalpha(letra))
+    {
+        return 0;
+    }
+    if(letra == ' ')
+    {
+        return 11;
+    }
+    if(letra == EOF)
+    {
+        return 10;
+    }
+}
+
+/*Informa si la palabra pasada por parametro es una palabra reservada.*/
+int esPalabraReservada(char *letra)
+{
+    
+}
+
+token tokenCorrespondiente(int estado)
+{
+    if(estado == 2)
+    {
+        return ID;
+    }
+
+    if(estado == 13)
+    {
+        return FDT;
+    }
 }
